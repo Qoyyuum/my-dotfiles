25f5b97158f1c5d82a9ed5d38c1572b787be6267
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2022-11-02 10:28:51 +0800

    :sparkles: choco neovim powershell

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+# Neovim setup
+New-Item -ItemType Directory -Path $env:LOCALAPPDATA/nvim/
+New-Item -ItemType Directory -Path $env:LOCALAPPDATA/nvim/autoload
+New-Item -ItemType Directory -Path $env:LOCALAPPDATA/nvim/autoload/plugged
+New-Item -ItemType SymbolicLink -Name $env:LOCALAPPDATA/nvim/autoload/plugged/init.vim -Value my-neovim-init/init.vim
+
+# Powershell Profile setup
+New-Item -ItemType SymbolicLink -Name $PROFILE -Value my-powershell
